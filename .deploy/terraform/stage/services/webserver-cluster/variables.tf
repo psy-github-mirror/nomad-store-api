e8c80bec5f3ffba22764b3b9853c825d15faf965
xabru <xabru@yandex.com>
2022-04-04 12:00:58 -0500

    chore:-destroy-resources

--- .deploy/terraform/stage/services/webserver-cluster/variables.tf
+++ .deploy/terraform/stage/services/webserver-cluster/variables.tf
@@ -1,18 +1,15 @@
 variable "stripe_secret_key" {
   description = "The Stripe secret key"
-  default     = "zzz"
   type        = string
 }
 
 variable "web_app_url" {
   description = "Web app url to redirect user after payment"
-  default     = "zzz"
   type        = string
 }
 
 variable "web_hook_secret" {
   description = "The Stripe webhook secret"
-  default     = "zzz"
   type        = string
 }
 
